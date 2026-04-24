48e3eecd32d080bb477c2a9bd8d1cc89782640c6
ekanshArora-123 <arorae@oregonstate.edu>
2026-04-24 10:24:12 -0700

    Added feedback button

--- modules/footer.php
+++ modules/footer.php
@@ -2,10 +2,14 @@
 <?php 
 if(!isset($contentOnly) || !$contentOnly): 
 ?>
-<button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#faqModal" style="position: fixed; right: 20px; bottom: 20px; z-index: 1001; background: rgb(195, 69, 0); color: #fff; font-size: 1rem; padding: .375rem .75rem; border-radius: 0.25rem; font-weight: normal;">
+<button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#faqModal" style="position: fixed; left: 20px; bottom: 20px; z-index: 1001; background: rgb(195, 69, 0); color: #fff; font-size: 1rem; padding: .375rem .75rem; border-radius: 0.25rem; font-weight: normal;">
     <i class="fas fa-question-circle me-1"></i>
     FAQ
 </button>
+<a href="https://docs.google.com/forms/d/e/1FAIpQLSdK6-dYdAUel_5yGWeJWiO7ptoXFscGZzRHhRI4FY7I1BDRog/viewform?" target="_blank" class="btn" type="button" style="position: fixed; right: 20px; bottom: 20px; z-index: 1001; background: rgb(195, 69, 0); color: #fff; font-size: 1rem; padding: .375rem .75rem; border-radius: 0.25rem; font-weight: normal;">
+    <i class="far fa-comment mr-1"></i>
+    Feedback
+</a>
 <?php include_once PUBLIC_FILES . '/modules/faq-modal.php'; ?>
 <footer>
     &copy;&nbsp;<?php echo date('Y'); ?> Oregon State University&nbsp;&nbsp;&nbsp;
